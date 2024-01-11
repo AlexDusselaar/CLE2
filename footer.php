3539cf38d79c91924df12f48230b6fcba7ea24a7
AlexDusselaar <alexdusselaar@gmail.com>
2024-01-11 13:23:47 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+    <footer>
+        <p>@LT-Interiors - 2024</p> <!-- jaartal via php?? -->
+    </footer>
+</body>
+<!-- vervangen door include-->
+
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LT Interiors</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav>
+        <img src="imgs/LT-interiors_logo.png" alt="logo">
+        <div class="navlinks">
+            <a class="logo-text" href="index.html">Interiors</a>
+            <a href="portfolio.php">Portfolio</a>
+            <a href="aboutus.php">Over ons</a>
+            <a href="reserveren.php">Reserveren</a>
+        </div>
+        <div class="login">
+            <a href="login.html">Inloggen</a>
+        </div>
+    </nav>
