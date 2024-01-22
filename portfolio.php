6558eba1df75c3509cfd077f176caa4e7a9fb3d6
AlexDusselaar <alexdusselaar@gmail.com>
2024-01-22 11:10:04 +0100

    portfolio page version 1

--- portfolio.php
+++ portfolio.php
@@ -1,8 +1,73 @@
 <?php
 require_once 'include/header.php';
 ?>
-<main>
-    <p>Lege pagina :(</p>
+<main id="portfolio">
+    <section id="logo">
+        <img src="imgs/LT-interiors_logo.png" alt="logo">
+        <div>
+            <h1>Interiors</h1>
+            <h2>portfolio</h2>
+        </div>
+    </section>
+    <section id="kantoor">
+        <img src="imgs/portfoliokantoor.png" alt="Kantoor">
+        <div>
+            <h3>Kantoor</h3>
+            <p>Bij dit project was de uitdaging
+                dat ik een kantoor ging inrichten
+                voor het merk ‘Parfumado’. Ik heb
+                mij hier laten inspireren door een
+                geur, namelijk kruidig en bloemig.</p>
+        </div>
+    </section>
+    <section id="tinyhouse">
+        <img src="imgs/portfoliotinyhouse.png" alt="Kantoor">
+        <div>
+            <h3>Tiny House</h3>
+            <p>De uitdaging van dit project lag bij
+                het tiny house zo duurzaam
+                mogelijk inrichten. Naast dat de
+                materialen duurzaam moesten
+                worden, moest je er ook in
+                kunnen wonen, slapen, koken en
+                genoeg opbergruimte hebben.</p>
+        </div>
+    </section>
+    <section id="kleuradvies">
+        <img src="imgs/portfoliokleuradvies.png" alt="Kantoor">
+        <div>
+            <h3>Kleuradvies</h3>
+            <p>Bĳ dit project was het de uitdaging om een kleuradvies
+                te maken voor een kinderrestaurant. Voordat ik dit
+                advies had gemaakt heb ik mĳ verdiept in de kleur
+                theorieën. Daarnaast heb ik een klein onderzoekje
+                gedaan naar de kleuren die peuters zien. Dit onderzoek
+                heb ik ook gedaan met vormen.</p>
+            <p>Ik heb een natuurlĳke, en vriendelĳke sfeer gecreëerd
+                Dit door de laag verzadigde kleuren. Uit onderzoek is
+                namelĳk gebleken dat kinderen onrustig worden van
+                felle kleuren. Daarnaast is het een trendy kleurgebruik
+                door de laag verzadigde kleuren. De ruimte oogt vrolĳk
+                en energiek. De natuurlĳke sfeer zie je terugkomen in
+                het warm roze bloemenbehang.</p>
+        </div>
+
+    </section>
+    <section id="horeca">
+        <img src="imgs/portfoliohoreca.png" alt="Kantoor">
+        <div>
+            <h3>Horeca</h3>
+            <p>Bĳ dit project moest ik een Vietnamees restaurant
+                inrichten. Daarbĳ moest de Vietnamese stĳl
+                gecombineerd worden met de luxe stĳl.</p>
+        </div>
+    </section>
+
+    <section id="reserveerbutton">
+        <h3>Wil je zelf ook advies?</h3>
+        <a href="reserveren.php">Reserverering maken</a>
+    </section>
+
 </main>
 
 <?php
